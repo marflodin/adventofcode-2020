168c561ba8cbe2f6a8cd69ac6f483e7f2b212fd2
martin.flodin <martin.flodin@klarna.com>
2020-12-08 18:22:18 +0100

    Minor refactoring, using try catch more for validation

--- src/day_4_part_2.py
+++ src/day_4_part_2.py
@@ -16,28 +16,22 @@ with open('../resources/input_day_4.txt', 'r') as input_file:
 
 
 def is_valid_height(value: str) -> bool:
-    if value is None:
-        return False
     try:
         match = re.match(r"([0-9]+)([a-z]+)", value, re.I).groups()
-    except AttributeError:
-        return False
-    if match[1] == 'cm':
-        return is_between(int(match[0]), 150, 193)
-    elif match[1] == 'in':
-        return is_between(int(match[0]), 59, 76)
-    else:
+        if match[1] == 'cm':
+            return is_between(int(match[0]), 150, 193)
+        elif match[1] == 'in':
+            return is_between(int(match[0]), 59, 76)
+    except (AttributeError, TypeError):
         return False
 
 
 def is_valid_colour(value: str) -> bool:
-    if value is None:
-        return False
     try:
         match = re.match(r"(#)([a-z0-9]+)", value, re.I).groups()
-    except AttributeError:
+        return len(match) == 2 and len(match[1]) == 6
+    except (AttributeError, TypeError):
         return False
-    return len(match) == 2 and len(match[1]) == 6
 
 
 def is_valid_eye_colour(value: str) -> bool:
@@ -46,10 +40,10 @@ def is_valid_eye_colour(value: str) -> bool:
     return value in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
 
 
-def is_between(value: int, min: int, max: int) -> bool:
+def is_between(value: int, min_value: int, max_value: int) -> bool:
     if value is None:
         return False
-    return min <= value <= max
+    return min_value <= value <= max_value
 
 
 def is_valid_passport_id(value: str) -> bool:
@@ -61,15 +55,15 @@ def is_valid_passport_id(value: str) -> bool:
 def safely_get_int(value: str) -> Optional[int]:
     try:
         return int(value)
-    except:
+    except TypeError:
         return None
 
 
 def is_valid_passport(item) -> bool:
     required_keys_map = {
-        'byr': {is_between(value=safely_get_int(item.get('byr', None)), min=1920, max=2002)},
-        'iyr': {is_between(value=safely_get_int(item.get('iyr', None)), min=2010, max=2020)},
-        'eyr': {is_between(value=safely_get_int(item.get('eyr', None)), min=2020, max=2030)},
+        'byr': {is_between(value=safely_get_int(item.get('byr', None)), min_value=1920, max_value=2002)},
+        'iyr': {is_between(value=safely_get_int(item.get('iyr', None)), min_value=2010, max_value=2020)},
+        'eyr': {is_between(value=safely_get_int(item.get('eyr', None)), min_value=2020, max_value=2030)},
         'hgt': {is_valid_height(value=item.get('hgt', None))},
         'hcl': {is_valid_colour(value=item.get('hcl', None))},
         'ecl': {is_valid_eye_colour(value=item.get('ecl', None))},
@@ -77,7 +71,6 @@ def is_valid_passport(item) -> bool:
     }
     for key in required_keys_map:
         if not next(iter(required_keys_map[key])):
-            print(f"invalid key {key} for element {item}")
             return False
     return True
 
